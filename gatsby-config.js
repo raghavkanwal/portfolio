c50db13a2786fc2019402cecb78bf89e2ebf9d39
Raghav Kanwal <raghav.kanwal@gmail.com>
2022-01-18 21:19:06 +0530

    S3 try-out

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
+    menuLinks: [
+      {
+        name: "home",
+        link: "/"
+      },
+      {
+        name: "page2",
+        link: "/page-2"
+      }
+    ],
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
