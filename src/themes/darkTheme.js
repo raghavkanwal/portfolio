79d2d693be9f648a837436391a9f5f3358e1fb72
Raghav Kanwal <raghav.kanwal@gmail.com>
2022-01-23 22:15:07 +0530

    Alternate theming

---
+++ src/themes/darkTheme.js
@@ -0,0 +1,15 @@
+export const darkTheme = {
+    headerBackground: `rgba(32,32,35,0.5)`,
+    bodyBackground: `rgba(32,32,35,0.92)`,
+    textColor: `rgba(255,255,255,0.92)`,
+    blurIntensity: `5px`,
+    pageContainerW: `960px`,
+    headerFontSize: `1.4rem`,
+    textFontSize: `0.9rem`,
+    gutterBottom: `2rem`,
+    teaseThemeColor: `#ffcc99`,
+    businessCardBackground: `rgba(255,255,255,0.08)`,
+    linkColor: `#81e6d9`
+}
+
+export default darkTheme
---
+++ src/themes/lightTheme.js
@@ -0,0 +1,14 @@
+const lightTheme = {
+    headerBackground: `#ffcc9980`,
+    bodyBackground: `rgba(255,255,255,0.92)`,
+    textColor: `rgb(32,32,35)`,
+    blurIntensity: `5px`,
+    pageContainerW: `960px`,
+    headerFontSize: `1.4rem`,
+    textFontSize: `0.9rem`,
+    gutterBottom: `2rem`,
+    teaseThemeColor: `rgba(32,32,35,1.0)`,
+    businessCardBackground: `#ffcc9980`,
+    linkColor: `#156c61`
+}
+export default lightTheme
