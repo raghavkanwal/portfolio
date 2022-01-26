c755b9244b5b363b5b087fe1a098b44696496386
Raghav Kanwal <raghav.kanwal@gmail.com>
2022-01-26 20:55:32 +0530

    Loading on Resume frame

--- src/components/resume/ResumeFrame.js
+++ src/components/resume/ResumeFrame.js
@@ -1,8 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
+import styled from "styled-components"
 
+const LoadingSpan = styled.span`
+    display:block;
+    text-align:center;
+    font-size: ${props => props.theme.textFontSize};
+    color: ${props => props.theme.textColor};
+    font-family: 'Roboto';
+    padding: 45vh 0;
+`;
 const ResumeFrame = () => {
+
+    const [isLoading, setLoading] = useState(true);
+
+    const handleLoading = () => {
+        setLoading(false)
+    }
+
     return (
-        <iframe title="Raghav Kanwal" src="https://registry.jsonresume.org/raghavkanwal?theme=pumpkin" style={{width: `100%`, border: 0, height: `100vh`}}></iframe>
+        <>
+            {isLoading && <LoadingSpan>Please wait...</LoadingSpan>}
+            <iframe onLoad={handleLoading} title="Raghav Kanwal" src="https://registry.jsonresume.org/raghavkanwal?theme=pumpkin" style={{width: `100%`, border: 0, height: `100vh`}}></iframe>
+        </>
     )
 }
 
